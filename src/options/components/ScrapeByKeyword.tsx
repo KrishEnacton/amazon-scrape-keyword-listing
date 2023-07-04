34cdce7b17ebdc9c2204d37d5c649fbe9c509060
nishu-murmu <nishumurmu017@gmail.com>
2023-07-04 15:03:38 +0530

    fix: adding tailwind classes

--- src/options/components/ScrapeByKeyword.tsx
+++ src/options/components/ScrapeByKeyword.tsx
@@ -42,34 +42,43 @@ const ScrapeByKeyword: React.FC<{}> = ({ }) => {
         }
     }
 
-    function getPercent(currentKeyword: string,keyword: string[]) {
+    function getPercent(currentKeyword: string, keyword: string[]) {
         const index = keyword.indexOf(currentKeyword)
         const percent: number = (index == -1 ? keyword.length - 1 : index / keyword.length) * 100
-        return percent < 0 ? 0 : percent
+        return percent < 0 ? 0 : percent > 100 ? 100 : percent
     }
 
     return (
-        <div>
-            <h3>Scrapping By Keywords</h3>
-            <form onSubmit={startScrapping}>
-                <div>
-                    Enter Keywords:
-                    <textarea
-                        name="asin"
-                        id="asin"
-                        required
-                        value={keyword}
-                        rows={5}
-                        onFocus={() => setStatus('ideal')}
-                        onChange={(e) => setKeyword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Start Scrapping</button>
-            </form>
-           {status !== 'ideal' && <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                <div className="bg-blue-600 h-2.5 rounded-full" style={{width: `${getPercent(currentKeyword, keyword.split("\n"))}%`}}></div>
-            </div>}
+        <div className="flex flex-col items-center justify-center">
+            <div className="max-w-md px-6 py-8 bg-white shadow-lg rounded-lg w-[600px]">
+                <h3 className="text-xl font-semibold mb-4">Scraping By Keywords</h3>
+                <form onSubmit={startScrapping} className="">
+                    <div className="mb-4">
+                        <label htmlFor="asin" className="block mb-2">Enter Keywords:</label>
+                        <textarea
+                            name="asin"
+                            id="asin"
+                            required
+                            value={keyword}
+                            rows={5}
+                            onFocus={() => setStatus('ideal')}
+                            onChange={(e) => setKeyword(e.target.value)}
+                            className="w-full p-2 border border-gray-300 rounded-md resize-none"
+                        ></textarea>
+                    </div>
+                    <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-md">Start Scraping</button>
+                </form>
+                {status !== 'ideal' && (
+                    <>
+                        <div className="w-full mt-4 bg-gray-200 rounded-full h-2.5 flex gap-x-2 justify-between">
+                            <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${getPercent(currentKeyword, keyword.split("\n"))}%` }}></div>
+                        </div>
+                        <div>{getPercent(currentKeyword, keyword.split("\n")).toFixed(0) + "%"}</div>
+                    </>
+                )}
+            </div>
         </div>
+
     )
 }
 
