5bb3154bba5c6bb8ffca333cfe3106a6bde931cf
nishu-murmu <nishumurmu017@gmail.com>
2023-08-22 14:53:03 +0530

    remove logs

--- src/options/layouts/AppLayout.tsx
+++ src/options/layouts/AppLayout.tsx
@@ -18,7 +18,6 @@ export const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children })
     })
   }, [])
 
-  console.log(isValidate, 'valid')
   if (loading) {
     return (
       <FullScreenLoader
