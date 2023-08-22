3967b33dc0212df72f9e67c5ed0338a091a2026b
nishu-murmu <nishumurmu017@gmail.com>
2023-08-22 14:56:30 +0530

    fix: adding toastify.

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -4,6 +4,7 @@ import ScrapeByKeyword from './components/ScrapeByKeyword'
 import 'react-toastify/dist/ReactToastify.css'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Login } from './components/Login'
+import { ToastContainer } from 'react-toastify'
 
 // Login with: 15677676824 / CNv$c8nzLkjb.
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/asin" element={<ScrapeByAsin />} />
         <Route path="/keyword" element={<ScrapeByKeyword />} />
       </Routes>
+      <ToastContainer />
     </HashRouter>
   )
 }
